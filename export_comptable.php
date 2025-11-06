341ec73c794e3a5ffb5f6fc2f5010bec23fe4f3f
Webmaster <web@consogarage.com>
2025-11-06 10:47:52 +0100

    author changed

--- export_comptable.php
+++ export_comptable.php
@@ -10,7 +10,7 @@ class Export_Comptable extends Module
         $this->name = 'export_comptable'; // doit correspondre au dossier
         $this->tab = 'administration';
         $this->version = '1.0.1';
-        $this->author = 'ChatGPT';
+        $this->author = 'Consogarage';
         $this->need_instance = 0;
         $this->bootstrap = true;
 
